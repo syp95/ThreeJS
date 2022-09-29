0f8d24417bb2a0a3b4ed0e75055ba1713cfd68e3
syp <sy95_055@naver.com>
2022-09-29 20:10:30 +0900

    create canvas background

--- src/index.js
+++ src/index.js
@@ -2,7 +2,37 @@ import * as THREE from 'three'
 import { WEBGL } from './webgl'
 
 if (WEBGL.isWebGLAvailable()) {
-  console.log(THREE)
+  //장면
+  const scene = new THREE.Scene()
+  scene.background = new THREE.Color(0x004fff)
+  // 카메라
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+
+  // 캔버스
+  // const canvas = document.querySelector('#canvas')
+
+  // 렌더러
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  document.body.appendChild(renderer.domElement)
+
+  function render(time) {
+    time *= 0.001 // convert time to seconds
+
+    // cube.rotation.x = time
+    // cube.rotation.y = time
+
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(render)
+  }
+  requestAnimationFrame(render)
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
   document.body.appendChild(warning)
